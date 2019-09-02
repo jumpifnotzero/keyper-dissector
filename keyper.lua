2008450f9072ffea6c58448dd0a97a67440161c3
James <james.w.mitchell@gmail.com>
2019-09-02 12:03:26 -0700

    sub-tree for each call; call id and parameter count shown

--- keyper.lua
+++ keyper.lua
@@ -5,12 +5,14 @@ local p_keyper = Proto("keyper", "KeyperPlus HSM")
 
 local pf_length = ProtoField.uint32 ("keyper.length", "Length", base.DEC)
 local pf_call_count = ProtoField.uint8 ("keyper.call_count", "Call Count", base.DEC)
-local pf_call_offset = ProtoField.uint32 ("keyper.call_offset", "Call Offset", base.HEX)
+local pf_call_id = ProtoField.uint32 ("keyper.call_id", "Call ID", base.HEX)
+local pf_param_count = ProtoField.uint32 ("keyper.param_count", "Parameter Count", base.DEC)
 
 p_keyper.fields = {
   pf_length,
   pf_call_count,
-  pf_call_offset,
+  pf_call_id,
+  pf_param_count,
 }
 
 
@@ -25,11 +27,23 @@ function p_keyper.dissector(tvbuf, pktinfo, root)
   local tree = root:add(p_keyper, tvbuf:range(0, pktlen))
 
   tree:add(pf_length, tvbuf:range(0, 4))
-  -- TODO: read this value; we need it to find the offsets..
-  tree:add(pf_call_count, tvbuf:range(4, 1))
 
-  -- TODO: this should be a loop for i = 0; i < call_count
-  tree:add(pf_call_offset, tvbuf:range(0x14, 4))
+  -- the number of calls in this frame
+  tree:add(pf_call_count, tvbuf:range(4, 1))
+  local call_count = tvbuf:range(4, 1):uint()
+
+  -- extract the offsets of each call
+  call_offsets = {}
+  for i=1, call_count do
+    call_offsets[i] = tvbuf:range(0x10 + (i * 4), 4):uint()
+  end
+
+  -- loop over each call
+  for i=1, call_count do
+    call_tree = tree:add("Call")
+    call_tree:add(pf_call_id, tvbuf:range(call_offsets[i], 4))
+    call_tree:add(pf_param_count, tvbuf:range(call_offsets[i] + 4, 4))
+  end
 
   return pktlen
 end
